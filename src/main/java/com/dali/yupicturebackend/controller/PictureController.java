823a0a3dc860b16309bba3b344f6367126bbd99e
86159 <lzc314594272@gmail.com>
2025-03-11 23:19:03 +0800

    修改标签

--- src/main/java/com/dali/yupicturebackend/controller/PictureController.java
+++ src/main/java/com/dali/yupicturebackend/controller/PictureController.java
@@ -224,8 +224,8 @@ public class PictureController {
         @GetMapping("/tag_category")
     public BaseResponse<PictureTagCategory> listPictureTagCategory() {
         PictureTagCategory pictureTagCategory = new PictureTagCategory();
-        List<String> tagList = Arrays.asList("热门", "搞笑", "生活", "高清", "艺术", "校园", "背景", "简历", "创意");
-        List<String> categoryList = Arrays.asList("模板", "电商", "表情包", "素材", "海报");
+        List<String> tagList = Arrays.asList("原神","Furina" );
+        List<String> categoryList = Arrays.asList("好图","黄图");
         pictureTagCategory.setTagList(tagList);
         pictureTagCategory.setCategoryList(categoryList);
         return ResultUtils.success(pictureTagCategory);

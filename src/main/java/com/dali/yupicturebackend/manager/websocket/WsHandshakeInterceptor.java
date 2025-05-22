a69d2abcaf98ebf71fdb179f1dc138086bc81390
86159 <lzc314594272@gmail.com>
2025-05-22 20:26:24 +0800

    添加协同编辑

---
+++ src/main/java/com/dali/yupicturebackend/manager/websocket/WsHandshakeInterceptor.java
@@ -0,0 +1,101 @@
+package com.dali.yupicturebackend.manager.websocket;
+
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import com.dali.yupicturebackend.manager.auth.SpaceUserAuthManager;
+import com.dali.yupicturebackend.manager.auth.model.SpaceUserPermissionConstant;
+import com.dali.yupicturebackend.manager.websocket.model.PictureEditResponseMessage;
+import com.dali.yupicturebackend.model.entity.Picture;
+import com.dali.yupicturebackend.model.entity.Space;
+import com.dali.yupicturebackend.model.entity.User;
+import com.dali.yupicturebackend.model.enums.SpaceTypeEnum;
+import com.dali.yupicturebackend.service.PictureService;
+import com.dali.yupicturebackend.service.SpaceService;
+import com.dali.yupicturebackend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+@Slf4j
+public class WsHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Resource
+    private UserService  userService;
+
+    @Resource
+    private PictureService pictureService;
+
+    @Resource
+    private SpaceService spaceService;
+    @Autowired
+    private SpaceUserAuthManager spaceUserAuthManager;
+
+
+    @Override
+    public boolean beforeHandshake(@NotNull ServerHttpRequest request, @NotNull ServerHttpResponse response, @NotNull WebSocketHandler wsHandler, @NotNull Map<String, Object> attributes) {
+        if (request instanceof ServletServerHttpRequest) {
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+
+            String pictureId = servletRequest.getParameter("pictureId");
+            if (StrUtil.isBlank(pictureId)) {
+                log.error("缺少图片参数，拒绝握手");
+                return false;
+            }
+            User loginUser = userService.getLoginUser(servletRequest);
+            if (ObjUtil.isEmpty(loginUser)) {
+                log.error("未登录，拒绝握手");
+                return false;
+            }
+            // 鉴权
+            Picture picture = pictureService.getById(pictureId);
+            if (picture == null) {
+                log.error("图片不存在，拒绝握手");
+                return false;
+            }
+            Long spaceId = picture.getSpaceId();
+            Space space =null;
+            if (spaceId != null) {
+                space = spaceService.getById(spaceId);
+                if (space == null) {
+                    log.error("空间不存在，拒绝握手");
+                    return false;
+                }
+                if (space.getSpaceType() != SpaceTypeEnum.TEAM.getValue()) {
+                    log.error("非团队空间，拒绝握手");
+                    return false;
+                }
+            }
+            List<String> permissionList = spaceUserAuthManager.getPermissionList(space, loginUser);
+            if (!permissionList.contains(SpaceUserPermissionConstant.PICTURE_EDIT)) {
+                log.error("用户没有权限，拒绝握手");
+                return false;
+            }
+            // 设置 attributes
+            attributes.put("user", loginUser);
+            attributes.put("userId", loginUser.getId());
+            attributes.put("pictureId", Long.valueOf(pictureId));
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+
+
+}
